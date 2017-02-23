b665b7139250badda75f7ed201181232ba0d0a5f
brian mclendon <brianmclendon@brians-MBP.home>
2017-02-23 12:31:09 -0500

    Adjusted css javascript and html structuring and changed a little of the css

--- giphy-js.js
+++ giphy-js.js
@@ -8,56 +8,47 @@ $(document).ready(function() {
 	///////
 	// MAIN WINDOW BUTTONS
 	///////
-	
-	$("#window_heightControl").click(function(event) {
+	$("#window-height-control").click(function(event) {
 		menuAI("toggle_height_size");
 	});
-
-	$("#window_widthControl").click(function(event) {
+	$("#window-width-control").click(function(event) {
 		menuAI("toggle_width_size");
 	});
 
 	///////
 	// FILE MENU BUTTONS
 	///////
-	$("#window_clearScreen").click(function(event) {
+	$("#window-clear-screen").click(function(event) {
 		menuAI("clearScreen");
 	});
-
-	$("#topicButtons_clear").click(function(event) {
+	$("#topic-buttons-clear").click(function(event) {
 		menuAI("clearButtons")
 	});
-
-	$("#topicButtons_reset").click(function(event) {
+	$("#topic-buttons-reset").click(function(event) {
 		renderTopicsArray();
 	});
 
 	///////
 	// HELP MENU BUTTONS
 	///////
-	$("#window_howTo").click(function(event) {
-		if (!$("#howToPop").length){
+	$("#window-how-to").click(function(event) {
+		if (!$("#how-to-pop").length){
   			popupWindows("howTo");
   		}
 	});
-
-	$("#window_about").click(function(event) {
-  		if (!$("#aboutPop").length){
+	$("#window-about").click(function(event) {
+  		if (!$("#about-pop").length){
   			popupWindows("about");
   		}
 	});
 
-
 	///////
 	// GIF MENU BUTTONS
 	///////
-
-
-	$("#typeButtons_submit").on("click", function(event) {
+	$("#type-buttons-submit").on("click", function(event) {
 		event.preventDefault();
 		NewButton("try");
 	});
-
 	$("#trending").on("click", function(event) {
 		event.preventDefault();
 		spawnPopups("trending");
@@ -73,15 +64,16 @@ function menuAI(whatToDo) {
 
 		var drag_is_on = $("#container").attr("data-content");
 
-		if(drag_is_on == "noDrag"){ 
+		if(drag_is_on == "no-drag"){ 
 			$("#container").attr("data-content", "drag");
 			$("#container").draggable({ disabled: false }); 
+
 		}else{ 
 			$("#container").attr("style", "left:0px; top:0px;");
 
 			$("#container").removeClass("ui-draggable").removeClass("ui-draggable-handle");
 			
-			$("#container").attr("data-content", "noDrag");
+			$("#container").attr("data-content", "no-drag");
 			$("#container").draggable({ disabled: true });
 		}
 	}
@@ -90,23 +82,22 @@ function menuAI(whatToDo) {
 		$("#container").toggleClass("container-H-closed").toggleClass("container-H-open");
 	}
 
-
 	if(whatToDo == "clearScreen"){
-		$( ".gifWindow" ).each(function() {
+		$( ".gif-window" ).each(function() {
   			$( this ).remove();
 		});
 	}
 
 	if(whatToDo == "clearButtons"){
-		$(".buttonHolder").empty();
+		$(".button-holder").empty();
 
 		var vr = $("<div>");
-		vr.addClass("fakeVR");
-		$(".buttonHolder").append(vr);
+		vr.addClass("fake-VR");
+		$(".button-holder").append(vr);
 		
 		var rowHolder = $("<div>");
-		rowHolder.addClass("rowHolder");
-		$(".buttonHolder").append(rowHolder);
+		rowHolder.addClass("row-holder");
+		$(".button-holder").append(rowHolder);
 
 		var row = $("<div>");
 		row.addClass("row");
@@ -121,12 +112,10 @@ function menuAI(whatToDo) {
 function NewButton(whatToDo) {
 	if(whatToDo == "try"){
 		var t = $("#topic-input").val().trim();
-		if(t != ""){ 
-			NewButton("create"); 	
-		}
+		if(t != ""){ NewButton("create"); }
 	}
-	if(whatToDo == "create"){
 
+	if(whatToDo == "create"){
 		var a = $("<button>");
 		var t = $("#topic-input").val().trim();
 		a.attr("data-name", t );
@@ -141,7 +130,7 @@ function NewButton(whatToDo) {
 		// console.log( $(".row:last-of-type" ).text() );
 
 		//if not
-		$(".buttonHolder").children(".rowHolder").children(".row").append(a);
+		$(".button-holder").children(".row-holder").children(".row").append(a);
 		//if so, 
 		// make a new row, then append to that
 	}
@@ -149,15 +138,15 @@ function NewButton(whatToDo) {
 
 function renderTopicsArray() {
 
-	$(".buttonHolder").empty();
+	$(".button-holder").empty();
 
 	var vr = $("<div>");
-	vr.addClass("fakeVR");
-	$(".buttonHolder").append(vr);
+	vr.addClass("fake-VR");
+	$(".button-holder").append(vr);
 
 	var rowHolder = $("<div>");
-	rowHolder.addClass("rowHolder");
-	$(".buttonHolder").append(rowHolder);
+	rowHolder.addClass("row-holder");
+	$(".button-holder").append(rowHolder);
 
 	var row = $("<div>");
 	row.addClass("row");
@@ -181,10 +170,11 @@ function spawnPopups(which) {
 		popupWindows("button", which);
 	}
 }
+
 function popupWindows(kind, which) {
 
 		var gW = $("<div>");
-		gW.addClass("gifWindow");
+		gW.addClass("gif-window");
 		var top = getRandomIntInclusive(0, (screen.height/1.2) );
 		var left = getRandomIntInclusive(0, (screen.width/2) );
 
@@ -202,11 +192,11 @@ function popupWindows(kind, which) {
 		$("body").append(gW);
 
 		var gW_TN = $("<div>");
-		gW_TN.addClass("gifWindow_topNav");
+		gW_TN.addClass("gif-window-top-nav");
 		gW.append(gW_TN);
 
 			var gW_T = $("<div>");
-			gW_T.addClass("gif_Top");
+			gW_T.addClass("gif-top");
 			var typeDisplay;
 			if(kind == "button"){
 				typeDisplay = which.substring(0,1).toUpperCase() + which.substring(1,which.length);
@@ -221,12 +211,12 @@ function popupWindows(kind, which) {
 			gW_TN.append(gW_T);
 
 			var gW_C = $("<button>");
-			gW_C.addClass("gif_close");
+			gW_C.addClass("gif-close");
 			gW_C.text("X");
 			gW_TN.append(gW_C);
 
 		var gW_GB = $("<button>");
-		gW_GB.addClass("gifButton");
+		gW_GB.addClass("gif-button");
 		gW.append(gW_GB);
 		
 		if(kind == "button"){
@@ -240,7 +230,7 @@ function popupWindows(kind, which) {
 				"Controls located in the File Menu.");
 			gW_GB.append(h);
 
-			gW.attr("id","howToPop");
+			gW.attr("id","how-to-pop");
 		}
 		if(kind == "about"){
 			var a = $("<div>");
@@ -250,7 +240,7 @@ function popupWindows(kind, which) {
 				"for Rutgers Coding Bootcamp by Brian McLedon");
 			gW_GB.append(a);
 
-			gW.attr("id","aboutPop");
+			gW.attr("id","about-pop");
 		}	
 
 		//interactions
@@ -263,11 +253,11 @@ function popupWindows(kind, which) {
 
 		gW_T.mousedown(function(event){ 
 			$("body").append( gW ); 
-			gW_T.toggleClass("gif_Top_Active");
+			gW_T.toggleClass("gif-top-active");
 		});
 
 		gW_T.mouseup(function(event){ 
-			gW_T.toggleClass("gif_Top_Active");
+			gW_T.toggleClass("gif-top-active");
 		});
 
 		gW_C.click(function(event){ 
@@ -293,7 +283,7 @@ function addGIF(search, placeWhere) {
 
     		var rand = getRandomIntInclusive(0,response.data.length-1);
 
-    		// console.log(response.data[0]);
+    		
 
 	    	var tempIMG = new Image();
 			tempIMG.src = response.data[rand].images.downsized_still.url; //saving this one like this so I can use the load function
@@ -305,9 +295,17 @@ function addGIF(search, placeWhere) {
 				placeWhere.parent().css('width', tempIMG.width).css('height', tempIMG.height + 30);
 			
 				var rating = $("<div>");
-				rating.addClass("gifRating");
+				rating.addClass("gif-rating");
 				rating.text( "Rated: "+response.data[0].rating.toUpperCase() );
 				placeWhere.append(rating);
+
+				var info = response.data[rand].slug.split("-");
+	    		info.pop();
+	    		var i= info.join(", #").toUpperCase();
+				var infoDisplay = $("<div>");
+				infoDisplay.addClass("gif-info");
+				infoDisplay.text("#"+i );
+				placeWhere.append(infoDisplay);
 
 			}, false);
 
@@ -325,13 +323,11 @@ function addGIF(search, placeWhere) {
 /*///////*/
 /*BEGIN */
 /*RE-USABLE JAVASCRIPT*/
-
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 /*END */
 /*RE-USABLE JAVASCRIPT*/
 /*///////*/
